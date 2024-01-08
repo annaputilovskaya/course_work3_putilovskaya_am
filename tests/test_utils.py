a532c32381191515729a20c38c12064b87ccbbaf
putilovskaya <putilovskayaa@mail.ru>
2024-01-08 23:53:00 +0300

    fix tests_utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -13,38 +13,38 @@ class TestUtils(unittest.TestCase):
 
     def test_show_operation(self):
         operation1 = {
-    "id": 441945886,
-    "state": "EXECUTED",
-    "date": "2019-08-26T10:50:58.294041",
-    "operationAmount": {
-      "amount": "31957.58",
-      "currency": {
-        "name": "руб.",
-        "code": "RUB"
-      }
-    },
-    "description": "Перевод организации",
-    "from": "Maestro 1596837868705199",
-    "to": "Счет 64686473678894779589"
-  }
+            "id": 441945886,
+            "state": "EXECUTED",
+            "date": "2019-08-26T10:50:58.294041",
+            "operationAmount": {
+                "amount": "31957.58",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Maestro 1596837868705199",
+            "to": "Счет 64686473678894779589"
+        }
         operation2 = {
-    "id": 587085106,
-    "state": "EXECUTED",
-    "date": "2018-03-23T10:45:06.972075",
-    "operationAmount": {
-      "amount": "48223.05",
-      "currency": {
-        "name": "руб.",
-        "code": "RUB"
-      }
-    },
-    "description": "Открытие вклада",
-    "to": "Счет 41421565395219882431"
-  }
+            "id": 587085106,
+            "state": "EXECUTED",
+            "date": "2018-03-23T10:45:06.972075",
+            "operationAmount": {
+                "amount": "48223.05",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Открытие вклада",
+            "to": "Счет 41421565395219882431"
+        }
 
         self.assertEqual(show_operation(operation1), "26.08.2019 Перевод организации\n"
-                                                    "Maestro 1596 83** **** 5199 -> Счет **9589\n"
-                                                    "31957.58 руб.")
+                                                     "Maestro 1596 83** **** 5199 -> Счет **9589\n"
+                                                     "31957.58 руб.")
         self.assertEqual(show_operation(operation2), "23.03.2018 Открытие вклада\n"
                                                      "Счет **2431\n"
                                                      "48223.05 руб.")
