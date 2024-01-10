a074540272e61baff4cb44e95c5921b70b5925c6
putilovskaya <putilovskayaa@mail.ru>
2024-01-10 23:40:04 +0300

    add tests for select_executed_operations and sort_operations_by_date functions to test_utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,6 @@
 import unittest
-from src.utils import modify_account_number, modify_date, show_operation
+from src.utils import modify_account_number, modify_date, show_operation, select_executed_operations, \
+    sort_operations_by_date
 
 
 class TestUtils(unittest.TestCase):
@@ -48,3 +49,84 @@ class TestUtils(unittest.TestCase):
         self.assertEqual(show_operation(operation2), "23.03.2018 Открытие вклада\n"
                                                      "Счет **2431\n"
                                                      "48223.05 руб.")
+
+    def test_select_executed_operations(self):
+        operations = [{
+            "id": 615064591,
+            "state": "CANCELED",
+            "date": "2018-10-14T08:21:33.419441",
+            "operationAmount": {
+                "amount": "77751.04",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод с карты на счет",
+            "from": "Maestro 3928549031574026",
+            "to": "Счет 84163357546688983493"
+        },
+            {
+                "id": 147815167,
+                "state": "EXECUTED",
+                "date": "2018-01-26T15:40:13.413061",
+                "operationAmount": {
+                    "amount": "50870.71",
+                    "currency": {
+                        "name": "руб.",
+                        "code": "RUB"
+                    }
+                },
+                "description": "Перевод с карты на счет",
+                "from": "Maestro 4598300720424501",
+                "to": "Счет 43597928997568165086"
+            }]
+        self.assertEqual(select_executed_operations(operations), [{
+                "id": 147815167,
+                "state": "EXECUTED",
+                "date": "2018-01-26T15:40:13.413061",
+                "operationAmount": {
+                    "amount": "50870.71",
+                    "currency": {
+                        "name": "руб.",
+                        "code": "RUB"
+                    }
+                },
+                "description": "Перевод с карты на счет",
+                "from": "Maestro 4598300720424501",
+                "to": "Счет 43597928997568165086"
+            }])
+
+    def test_sort_operation_by_date(self):
+        operations_list = [{
+            "id": 441945886,
+            "state": "EXECUTED",
+            "date": "2019-08-26T10:50:58.294041",
+            },
+            {
+                "id": 587085106,
+                "state": "EXECUTED",
+                "date": "2018-03-23T10:45:06.972075"
+            },
+            {
+            "id": 41428829,
+            "state": "EXECUTED",
+            "date": "2019-07-03T18:35:29.512364"
+            }]
+        sorted_operations_list = [{
+            "id": 441945886,
+            "state": "EXECUTED",
+            "date": "2019-08-26T10:50:58.294041",
+            },
+            {
+                "id": 41428829,
+                "state": "EXECUTED",
+                "date": "2019-07-03T18:35:29.512364"
+            },
+            {
+                "id": 587085106,
+                "state": "EXECUTED",
+                "date": "2018-03-23T10:45:06.972075"
+            }]
+
+        self.assertEqual(sort_operations_by_date(operations_list), sorted_operations_list)
